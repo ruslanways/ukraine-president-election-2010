849ea95de8cc4de50a61918639e777076668d6c7
Ruslan Mansurov <ruslanways@gmail.com>
2023-01-12 11:59:43 +0100

    added favicons

--- test.php
+++ test.php
@@ -4,6 +4,10 @@ $out = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://w
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <link rel="stylesheet" href="/style.css">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
 <script src="/script.js"></script>
 <title>Вибір</title>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
